03c3c34880c3fb70b80b4a0d699679eb9dd77e50
LukasG62 <lukas.grando.62@gmail.com>
2023-01-06 18:15:57 +0100

    ajout fonction getAction

--- Back Evanerd/controller/baseController.php
+++ Back Evanerd/controller/baseController.php
@@ -1,16 +1,28 @@
 <?php
+include_once "includes/maLibUtils.php";
+
+DEFINE("API_NAME", "EVANERD API");
+DEFINE("VERSION", "1.0");
+
 
 function getAuthHeader() {
     return valider("HTTP_AUTHTOKEN" , "SERVER");
 }
 
 /**
- * Permet de récupère la chaine de requête envoyer par le client
+ * Permet de récupèrer la chaine de requête envoyer par le client
  * @return array
  */
 function getRequestParams() {
     parse_str($_SERVER['QUERY_STRING'], $query);
     return $query;
+}
+
+/**
+ * Permet de récupèrer le type de requête (GET POST PUT PATCH DEL )
+ */
+function getRequestType() {
+    return valider("REQUEST_METHOD" , "SERVER");
 }
 
 /**
@@ -22,6 +34,39 @@ function getUriSegments()
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $uri = explode('/', $uri);
     return $uri;
+}
+/**
+ * Renvoie le code de la route afin de pouvoir effecuter les traitements
+ * @param string $request le champs rêquete de la qs
+ * @param string $type type de la rêquete
+ * @param array &$idTabs passage d'un tableau par références 
+ *               des id trouver dans la route
+ * 
+ * @return string
+ */
+function getAction($request, $type, &$idTabs) {
+    if($request === false) return "NOT_A_ACTION";
+
+    $action = "$type ";
+    $requestSegments = explode("/", $request);
+    foreach($requestSegments as $segment) {
+        if(is_id($segment)) {
+            $action .= "/ID";
+            $idTabs[] = intval($segment);
+        }
+        else {
+            if(!empty($segment)) $action .= "/$segment";
+        }
+    }
+    return $action;
+}
+
+/**
+ * Génére les informations à propos de l'api
+ * (sorte d'entête de reponse de l'api)
+ */
+function genereAPIInfo() {
+    return array("apiname" => API_NAME, "version" => VERSION);
 }
 
 /**
@@ -37,8 +82,24 @@ function sendResponse($data, $httpHeaders = array())
             header($httpHeader);
         }
     }
-    echo $data;
+    
+    echo json_encode($data);
     exit;
+}
+
+/**
+ * Renvoie une réponse d'erreur avec le status passé en paramètre
+ * @param string $message le message d'erreur
+ * @param int $status le status HTTP 
+ */
+function sendError($message, $status) {
+    $data = genereAPIInfo();
+    $data["status"] = $status;
+    $data["error"] = $message;
+    $httpHeaders = [getStatusHeader($status)];
+
+    sendResponse($data, $httpHeaders);
+
 }
 
 /**
@@ -46,7 +107,7 @@ function sendResponse($data, $httpHeaders = array())
  * @param int $status
  * @Note Par defaut cette fonction renvoie le status 200
  */
-function getStatusHeaders($status = 200) {
+function getStatusHeader($status = 200) {
     switch($status) {
         case 200: return "HTTP/1.0 200 OK";
         case 201: return "HTTP/1.0 201 Created";
