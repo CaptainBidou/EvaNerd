986e7d348aa73d330b590023605ddbe6304d2538
LukasG62 <lukas.grando.62@gmail.com>
2023-01-07 20:33:40 +0100

    listUser fonctionnelle

--- Back Evanerd/controller/controller.php
+++ Back Evanerd/controller/controller.php
@@ -1,44 +1,37 @@
 <?php
 include_once "baseController.php";
+include_once "model/model.php";
 
+/**
+ * Liste les utilisateur et renvoie la réponse sous format JSON dans le flux STDIN
+ */
 function listUsers($data, $queryString) {
-    $users = array();
-    $momo = array("id" => 1,
-        "firstname" => "Maurice",
-        "lastName" => "Monticule",
-        "mail" => "momo@gmail.com",
-        "sex" => 0,
-        "age" => 19,
-        "studies" => "Centrale Lille IG2I",
-        "photo" => "www.example/users/1.png");
+    $idRole = null;
 
-    $jp = array("id" => 2,
-        "firstname" => "Jean Pierre",
-        "lastName" => "Barrebasse",
-        "mail" => "jpBasse@gmail.com",
-        "sex" => 2,
-        "age" => 34,
-        "studies" => "Ecole du rire",
-        "photo" => "www.example/users/2.png");
-    // Fixture en attendant le model.php
-    if(isset($queryString["idRole"])) {
-        $users = array();
-        $users[] = $momo;
-        $data["users"] = $users;
-        echo json_last_error();
-        sendResponse($data, [getStatusHeader(200)]);
+    if($idRole = valider("idRole", $queryString)) {
+        if(!is_id($idRole)) {
+            sendError("identifiant role attendu !", 400);
+            return;
+        }
+    }
+    $data["users"] = getUsers($idRole);
+    if(count($data["users"]) == 0) {
+        sendError("Aucun enregistrement trouvé : idRole invalide !", 400);
         return;
     }
-    else {
-        $users = array();
-        $users[] = $momo;
-        $users[] = $jp;
-        $data["users"] = $users;
-        sendResponse($data, [getStatusHeader(200)]);
-        return;
-    }
+
+    sendResponse($data, [getStatusHeader(201)]);
 }
 
+/**
+ * Créer un utilisateur est renvoie la réponses sous format JSON dans le flux STDIN
+ * @param $data Tableau de réponse
+ * @param $querystring chaine de requête
+ * @return void
+ */
+function postUser($data, $queryString) {
+    sendError("NOT IMPLEMENTED YET", 404);
+}
 
 function notAction($data) {
     sendResponse($data, [getStatusHeader(200)]);
