313744195652d94c4f7b810b1b97e1a27cb6f53d
LukasG62 <lukas.grando.62@gmail.com>
2023-01-10 11:49:34 +0100

    ajout prototypes fonctions + listGroupMessages

--- Back Evanerd/controller/controller.php
+++ Back Evanerd/controller/controller.php
@@ -5,6 +5,8 @@ include_once "includes/maLibSecurisation.php";
 
 /**
  * Permet à un utilisateur de s'identifier
+ * @param array Tableau de donnée à completer et envoyé
+ * @param array queryString sous forme de tableau
  */
 function authUser($data, $queryString) {
     if($tel = valider("tel", $queryString))
@@ -24,6 +26,8 @@ function authUser($data, $queryString) {
 
 /**
  * Liste les utilisateur et renvoie la réponse sous format JSON dans le flux STDIN
+ * @param array Tableau de donnée à completer et envoyé
+ * @param array queryString sous forme de tableau
  */
 function listUsers($data, $queryString) {
     $idRole = null;
@@ -34,7 +38,7 @@ function listUsers($data, $queryString) {
             return;
         }
     }
-    $data["users"] = getUsers($idRole);
+    $data["users"] = selectUsers($idRole);
     if(count($data["users"]) == 0) {
         sendError("Aucun enregistrement trouvé : idRole invalide !", HTTP_NOT_FOUND);
         return;
@@ -42,6 +46,7 @@ function listUsers($data, $queryString) {
 
     sendResponse($data, [getStatusHeader(HTTP_CREATED)]);
 }
+
 /**
  * Renvoie l'utilisateur sous format JSON dans le flux STDIN
  */
@@ -52,7 +57,7 @@ function sendUser($data, $idTabs, $authKey) {
     if(authToId($authKey) == $idTabs[0]) {
         $me = true;
     }
-    $user = getUser($idTabs[0], $me);
+    $user = selectUser($idTabs[0], $me);
     if(count($user) == 0) {
         sendError("Aucun enregistrement trouvé : id invalide !", HTTP_NOT_FOUND);
         return;
@@ -102,22 +107,205 @@ function postUser($data, $queryString) {
         $password = password_hash($plainPassword, PASSWORD_BCRYPT, ["cost"=>10]);
         if($imageData != null) $image = "image.png";
 
-        $idUser = createUser($firstName, $lastName, $mail, $tel, $password, $age, $studies, $sex, $image);
-        $data["user"] = getUser($idUser)[0];
+        $idUser = insertUser($firstName, $lastName, $mail, $tel, $password, $age, $studies, $sex, $image);
+        $data["user"] = selectUser($idUser)[0];
         sendResponse($data, [getStatusHeader(HTTP_CREATED)]);
     }
     sendError("Requête Invalide",400);
 }
 
+/**
+ * Renvoie la liste des groupes
+ * @param array $data tableau à completer et envoyé
+ * @param array $queryString paramètre de requête
+ * @param string $authKey Token d'identification de l'utilisateur
+ */
 function listGroups($data, $queryString, $authKey) {
     if($authKey) {
+        // TODO : list group where user have permission
         $idUser = authToId($authKey);
-        $data["groups"] = getGroups($idUser);
+        $data["groups"] = selectGroups($idUser);
         sendResponse($data, [getStatusHeader(HTTP_OK)]);
     }
     sendError("Vous devez être connecté !", HTTP_FORBIDDEN);
 }
+/**
+ * Renvoie la liste des message d'un groupe
+ */
+function listGroupMessage($data, $authKey, $idTabs) {
+    if($authKey)
+    if(count($idTabs) == 1) {
+        $gid  = $idTabs[0];
+        $idUser = authToId($authKey);
+        if(isInGroup($idUser, $gid) || count(haveGroupPermission($idUser, $gid))) {
+            $data["groupId"] = $gid;
+            $data["messages"] = selectGroupMessages($gid);
+            sendResponse($data, [getStatusHeader(HTTP_OK)]);
+        }
+        // TODO : error messages
+        
+        return;
+    }
 
+    sendError("Il faut vous identifié !", HTTP_FORBIDDEN);
+}
+
+/**
+ * Modifie un utilisateur
+ * @param array $data tableau à completer et envoyé
+ * @param array $queryString paramètre de requête
+ * @param string $authKey Token d'identification de l'utilisateur
+ */
+function putUser($data, $idTabs, $authKey) {
+
+}
+
+/**
+ * Ajout un instrument à l'utilisateur connecté
+ * @param array $data tableau à completer et envoyé
+ * @param array $queryString paramètre de requête
+ * @param string $authKey Token d'identification de l'utilisateur
+ */
+function postUserInstrument($data, $idTabs, $authKey, $queryString) {
+
+}
+
+/**
+ * Ajout un role à l'utilisateur connecté
+ * @param array $data tableau à completer et envoyé
+ * @param array $queryString paramètre de requête
+ * @param string $authKey Token d'identification de l'utilisateur
+ */
+function postUserRole($data, $idTabs, $authKey, $queryString) {
+
+}
+
+/**
+ * Vérifie l'email d'un utilisateur
+ * @param array $data tableau à completer et envoyé
+ * @param array $queryString paramètre de requête
+ * @param string $authKey Token d'identification de l'utilisateur
+ */
+function verifMail($data, $idTabs, $authKey) {
+
+}
+
+/**
+ * Supprime un instrument de l'utilisateur connecté
+ * @param array $data tableau à completer et envoyé
+ * @param array $queryString paramètre de requête
+ * @param string $authKey Token d'identification de l'utilisateur
+ */
+function delUserInstrument($data, $idTabs, $authKey, $queryString) {
+
+}
+
+/**
+ * 
+ */
 function notAction($data) {
     sendResponse($data, [getStatusHeader(HTTP_OK)]);
+}
+
+
+function postUserAchievement($data, $idTabs, $authKey, $queryString) {
+
+}
+
+function postRole($data, $idTabs, $authKey, $queryString) {
+
+}
+
+function delUserAchievement($data, $idTabs, $authKey, $queryString) {
+
+}
+
+function delUserRole($data, $idTabs, $authKey, $queryString) {
+
+}
+
+
+function listRoles($data, $queryString) {
+
+}
+
+
+function putRoles($data, $idTabs, $authKey, $queryString) {
+
+}
+
+function postRoles($data, $authKey, $queryString) {
+
+}
+
+function listInstruments($data, $queryString) {
+
+}
+
+function putInstruments($data, $idTabs, $authKey, $queryString) {
+
+}
+
+function postInstruments($data, $authKey, $queryString) {
+
+}
+
+function listAchievements($data, $queryString) {
+
+}
+
+function putAchievements($data, $idTabs, $authKey, $queryString) {
+
+}
+
+function postAchievements($data, $authKey, $queryString) {
+
+}
+
+function listGroupsPerms($data, $idTabs, $authKey) {
+
+}
+
+function listGroupsMessages($data, $idTabs, $authKey) {
+
+}
+
+function listGroupsReacts($data, $idTabs, $authKey) {
+
+}
+
+function createUserGroups($data, $authKey, $queryString) {
+
+}
+
+function addUsersGroups($data, $authKey) {
+
+}
+
+function postMessagesGroups($data, $idTabs, $authKey, $queryString) {
+
+}
+
+function listPosts($data, $authKey) {
+
+}
+
+function listPostsReacts($data, $idTabs, $authKey) {
+
+}
+
+function listPostsMessages($data, $idTabs, $authKey) {
+
+}
+
+function listAgendas($data, $authKey) {
+
+}
+
+function listAgendaEvents($data, $idTabs, $authKey) {
+
+}
+
+function listAgendaEventCalls($data, $idTabs, $authKey) {
+
 }
