2f54f1600f53cfae3e27dd2ce1e5b8570112cb37
TRETomas <99128577+TRETomas@users.noreply.github.com>
2023-01-10 11:27:26 +0100

    switch case index avec fonctions

--- Back Evanerd/index.php
+++ Back Evanerd/index.php
@@ -15,9 +15,13 @@ else $request = false;
 $action = getAction($request, $requestType, $idTabs);
 
 switch($action) {
+
+    /* Authentification */
     case "POST /auth":
         authUser($data, $queryString);
         break;
+
+    /* Users */
     case "GET /users":
         listUsers($data, $queryString);
         break;
@@ -25,15 +29,135 @@ switch($action) {
     case "GET /users/ID":
         sendUser($data, $idTabs, $authKey);
         break;
+    
+    case "PUT /users/ID":
+        putUser($data, $idTabs, $authKey, $queryString);
+        break;
+
     case "POST /users":
         postUser($data, $queryString);
         break;
 
+    case "POST /users/instruments":
+        postInstrument($data, $idTabs, $authKey, $queryString);
+        break;
+
+    case "POST /users/achievement":
+        postAchievement($data, $idTabs, $authKey, $queryString);
+        break;
+
+    case "POST /users/ID/roles":
+        postRole($data, $idTabs, $authKey, $queryString);
+        break;
+
+    case "POST /users/verify":
+        verifMail($data, $idTabs, $authKey);
+        break;
+
+    case "DEL /users/instruments":
+        delInstrument($data, $idTabs, $authKey, $queryString);
+        break;
+    
+    case "DEL /users/achievements":
+        delAchievement($data, $idTabs, $authKey, $queryString);
+        break;
+    
+    case "DEL /users/roles":
+        delRole($data, $idTabs, $authKey, $queryString);
+        break;
+
+    /* Roles */
+    case "GET /roles":
+        listRoles($data, $queryString);
+        break;
+    
+    case "PUT /roles/ID":
+        putRoles($data, $idTabs, $authKey, $queryString);
+        break;
+    
+    case "POST /roles":
+        postRoles($data, $authKey, $queryString);
+        break;
+    
+    /* Instruments */
+    case "GET /instruments":
+        listInstruments($data, $queryString);
+        break;
+
+    case "PUT /instruments/ID":
+        putInstruments($data, $idTabs, $authKey, $queryString);
+        break;
+
+    case "POST /instruments":
+        postInstruments($data, $authKey, $queryString);
+        break;
+
+    /* Achievements */
+    case "GET /achievements":
+        listAchievements($data, $queryString);
+        break;
+    
+    case "PUT /achievements/ID":
+        putAchievements($data, $idTabs, $authKey, $queryString);
+        break;
+    
+    case "POST /achievements":
+        postAchievements($data, $authKey, $queryString);
+
+    /* Groups */
     case "GET /groups":
         listGroups($data, $queryString, $authKey);
         break;
+
+    case "GET /groups/ID/permissions":
+        listGroupsPerms($data, $idTabs, $authKey);
+        break;
+
     case "GET /groups/ID/messages":
-        listGroupMessage($data, $queryStringn, $authKey, $idTabs);
+        listGroupsMessages($data, $idTabs, $authKey);
+        break;
+    
+    case "GET /groups/ID/messages/ID":
+        listGroupsReacts($data, $idTabs, $authKey);
+        break;
+
+    case "POST /groups":
+        createUserGroups($data, $authKey, $queryString);
+        break;
+    
+    case "POST /groups/ID/users/ID":
+        addUsersGroups($data, $authKey);
+        break;
+    
+    case "POST /groups/ID/messages":
+        postMessagesGroups($data, $idTabs, $authKey, $queryString);
+        break;
+
+    /* Posts */
+    case "GET /posts":
+        listPosts($data, $authKey);
+        break;
+
+    case "GET /posts/ID/reactions":
+        listPostsReacts($data, $idTabs, $authKey);
+        break;
+    
+    case "GET /posts/ID/messages":
+        listPostsMessages($data, $idTabs, $authKey);
+        break;
+    
+    /* Agendas */
+
+    case "GET /agendas":
+        listCalendriers($data, $authKey);
+        break;
+    
+    case "GET /agendas/ID":
+        listEventsCalendriers($data, $idTabs, $authKey);
+        break;
+
+    case "GET /agendas/ID/event/ID/calls":
+        listAppels($data, $idTabs, $authKey);
         break;
 
     default:
