500a2840633a46ed8bb382db1184181e9ebc9478
LukasG62 <lukas.grando.62@gmail.com>
2023-01-28 19:46:46 +0100

    feat(maLibGD2) ajout fonction defaultPicture

--- Back Evanerd/includes/maLibGD2.php
+++ Back Evanerd/includes/maLibGD2.php
@@ -25,6 +25,36 @@ function hexToRGB($hex, &$r, &$g, &$b) {
     $g = hexdec($matches[2]);
     $b = hexdec($matches[3]);
 }
+
+/**
+ * Fonction retournant les info d'un texte à incruster en fonction 
+ * de la taille de l'image cible
+ * @param int $imgL largeur de l'image cible
+ * @param int $imgH hauteur de l'image cible
+ * @param string $text texte à écrire sur l'image
+ * @param string $font la police utilisé
+ * @param $fontSizeMax taille de police maximum sur l'image cible
+ * @param $step degrée de diminution de la police (doit être multiple de $fontSizeMax) 
+ */
+function getTexteInfo($imgL, $imgH, $text, $font, $fontSizeMax = 180, $step = 5) {
+	$fontSize = $fontSizeMax;
+	$textBox = imagettfbbox($fontSizeMax, 0, $font, $text);
+	$textWidth = abs($textBox[4] - $textBox[0]);
+	$textHeight= abs($textBox[5] - $textBox[1]);
+	
+	while( $textWidth > $imgL || $textHeight > $imgH) {
+		// on abaisse la police d'écriture
+		$fontSize = $fontSize-5;
+		// on recalcule la taille
+		$textBox = imagettfbbox($fontSize, 0, $font, $text);
+		$textWidth = abs($textBox[4] - $textBox[0]);
+		$textHeight= abs($textBox[5] - $textBox[1]);
+	}
+
+	return ["fontSize" => $fontSize, "height" => $textHeight, "width" => $textWidth];
+
+}
+
 /**
  * Génere la photo de profile par défaut d'un utilisateur sous format png
  * @param string $output fichier de sortie de l'image
@@ -32,8 +62,50 @@ function hexToRGB($hex, &$r, &$g, &$b) {
  * @param string $lastName nom de famille de l'utilisateur
  * @return void
  */
-function defaultPicture($ouput, $firstName, $lastName) {
+function defaultPicture($output, $firstName, $lastName) {
+    // Tirage d'une couleur aléatoire
+    $bgColor = array_column(COLOR::cases(), "value");
+    $bgHex = $bgColor[array_rand($bgColor)];
+    hexToRGB($bgHex, $r, $v, $b);
 
+    // Créer une nouvelle image de 450x450 pixels
+    $image = imagecreate(450, 450);
+
+    // Définir la couleur de fond en violet
+    $bg = imagecolorallocate($image, $r, $v, $b);
+
+    // Remplir l'image avec la couleur de fond
+    imagefill($image, 0, 0, $bg);
+
+    // Définir la couleur du texte en blanc
+    $textColor = imagecolorallocate($image, 255, 255, 255);
+    // Définir la couleur du contour en noir
+    $borderColor = imagecolorallocate($image, 0, 0, 0);
+    // Définir la police à utiliser pour le texte
+    putenv('GDFONTPATH=' . realpath('./font'));
+	$font = realpath("./font/arial.ttf");
+    // Obtenir les initiales du nom et prénom
+    $initials = strtoupper($firstName[0] . $lastName[0]);
+    // Calcul taille cu texte
+    $textInfo = getTexteInfo(400,400, $initials, $font, 380, 1);
+    $textHeight = $textInfo["height"];
+    $textWidth = $textInfo["width"];
+    $fontSize = $textInfo["fontSize"];
+
+    // Ajouter le text à l'image
+    imagettftext($image, $fontSize, 0, 450/2 - $textWidth/2, 450/2 + $textHeight/2, $borderColor, $font, $initials);
+    imagettftext($image, $fontSize, 0, 450/2 - $textWidth/2, 450/2 + $textHeight/2, $textColor, $font, $initials);
+    // Ajouter un contour autour du texte
+    // Enregistrer l'image
+    imagepng($image, $output);
+    // Libérer la mémoire utilisée par l'image
+    imagedestroy($image);
 }
 
+/*
+$alphabet = range("A", "Z");
+$a = $alphabet[array_rand($alphabet)];
+$b = $alphabet[array_rand($alphabet)];
+defaultPicture("test.png", $a, $b);
+*/
 ?>
