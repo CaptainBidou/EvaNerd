c8b02c5b5a617a2be3642ba18035784e92e06246
CaptainBidou <tomas.salvado_robalo@ig2i.centralelille.fr>
2023-02-05 17:04:30 +0100

    connexion et messages

--- Front Evanerd/biblio_composants.js
+++ Front Evanerd/biblio_composants.js
@@ -122,7 +122,7 @@ var JMessageFleche =$("<img>").attr("src","Ressources/Message/arrow.png").addCla
 var JMessageReglage=$("<img>").attr("src","Ressources/Message/reglage.png").addClass("Message-Reglage");
 var JMessageEpingle=$("<img>").attr("src","Ressources/Message/epingle.png").addClass("Message-Epingle");
 var JMessageParticipant=$("<p>").addClass("Message-Participant");
-var JMessage=$("<div>").addClass("Message").data("attribut","divMessage");
+var JMessage=$("<div>").addClass("Message").data("attribut","divMessage").attr("id","DivMessage");
 var JMessageInput=$("<textarea>").attr('type','text').addClass("form-control Message-Input").attr("placeholder","Votre message");
 var JMessageSend=$("<img>").addClass("Message-Send ").attr("src","Ressources/Message/send.png").on("click",function(){return null;});
 var JMessageDown=$("<div>").addClass("Message-Down");
@@ -143,7 +143,13 @@ var JMessageActifRep=$("<img>").addClass("Actif-Rep").attr("src","Ressources/Mes
 var JMessageActifEpingle=$("<img>").addClass("Actif-Epingle").attr("src","Ressources/Message/epingle.png");
 var JMessageActifContent=$("<p>").addClass("Actif-content");
 
-
+//variables pour la connexion
+var JConnexion = $("<div>").addClass("divFormConnexion");
+var JConnexionTelephone=$("<input>").addClass("form-control Connexion-phone").attr("placeholder","Téléphone").attr("id","tel");
+var JConnexionPwd=$("<input>").addClass("text").addClass("form-control Connexion-pwd").attr("placeholder","Mot de passe").attr("id","pwd");
+var JConnexionSubmit=$("<button>").addClass("btn btn-danger Connexion-Submit").html("Se connecter").on("click",function(context){Connexion();});
+var JConnexionP=$("<p>").addClass("Connexion-p");
+var JConnexionTitre=$("<h1>").addClass("Connexion-titre");
 
 /************************************************************************/
 /*                 DECLARATION DES FONCTIONS                           */
@@ -408,30 +414,7 @@ function JCreerConv(Reponse){
     $("#page").append(JCloneConv);
 }
 
-/**
- * 
- * @param {*} Reponse est un json contenant un tableau "message " de json dans lesquels il y a le nom de celui qui a envoyé le message, sa couleur son message sa pdp cette liste va donc du plus ancien message au plus récent 
- * 
- * 
- * Reponse = {'message':[
- * {nom:Mathieu,prenom:Somet,message:"salut les geeks",profile:"Ressources/Test/profile2.jpg"},
- * {nom:Norman,prenom;Thavaux,message"moi aussi jaime les enfants",profile:"Ressources/Test/profile3.jpg"}
- * 
- * 
- * 
- * ]}
- * 
- * 
- * 
- * 
- */
-function JAfficherMessageConv(Reponse){
 
-
-
-
-
-}
 
 
 
@@ -844,28 +827,56 @@ function JCreerMessage(Reponse){
     
     $(JCloneMessageHeader).append([JCloneMessageFleche,JCloneMessageParticipant,JCloneMessageReglage,JCloneMessageEpingle]);
     $("#page").append(JCloneMessageHeader);
-
+    $("#page").append(JCloneMessage);
 JCloneMessageDown.append([JCloneMessageSend,JCloneMessageInput]);
 $("#page").append(JCloneMessageDown);
 
-    
+    JCreerMessageParticipant(Reponse,JCloneMessage);
     
 
 }
 
-function JCreerMessageParticipant(Reponse)
+function JCreerMessageParticipant(Reponse,div)
+{
+
+    var JCloneMessageParticipantDiv=JMessageParticipantDiv.clone(true,true);
+    var JCloneMessageParticipantProfile=JMessageParticipantProfile.clone(true,true).attr("src",Reponse.banner);
+    var JCloneMessageParticipantTitre=JMessageParticipantTitre.clone(true,true).text(Reponse.firstName+ " "+Reponse.lastName);
+    var JCloneMessageParticipantRep=JMessageParticipantRep.clone(true,true);
+    var JCloneMessageParticipantEpingle=JMessageParticipantEpingle.clone(true,true);
+    var JCloneMessageParticipantContent=JMessageParticipantContent.clone(true,true).text(Reponse.content);
+
+
+
+    JCloneMessageParticipantDiv.append([JCloneMessageParticipantTitre,JCloneMessageParticipantRep,JCloneMessageParticipantEpingle,JCloneMessageParticipantContent]);
+$(div).append([JCloneMessageParticipantProfile,JCloneMessageParticipantDiv]);
+
+}
+
+function JCreerMessageActif(Reponse,div)
 {
 
 
 
-
 }
 
 
 
+function JCreerConnexion(){
+
+    var JCloneTitre=JConnexionTitre.clone(true,true).text("Connexion");
+    var JCloneLegendTel=JConnexionP.clone(true,true).text("Téléphone");
+    var JCloneLegendPwd=JConnexionP.clone(true,true).text("Mot de Passe");
+    var JCloneConnexion =  JConnexion.clone(true,true);
+    var JCloneConnexionTelephone= JConnexionTelephone.clone(true,true);
+    var JCloneConnexionPwd=JConnexionPwd.clone(true,true);
+    var JCloneConnexionSubmit=JConnexionSubmit.clone(true,true);
+
+    JCloneConnexion.append([JCloneTitre,JCloneLegendTel,JCloneConnexionTelephone,JCloneLegendPwd,JCloneConnexionPwd,JCloneConnexionSubmit]);
+    $("#page").append(JCloneConnexion);
 
 
-
+}
 
 
 
