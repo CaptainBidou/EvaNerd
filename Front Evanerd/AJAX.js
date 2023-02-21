65adc12c29debb0cbcc99f85ee0be1441be7fad9
TRETomas <viixyy2@gmail.com>
2023-02-21 15:21:03 +0100

    pin msg conv

--- Front Evanerd/AJAX.js
+++ Front Evanerd/AJAX.js
@@ -662,6 +662,16 @@ function AddMsgConv($gid, $informations){
 
 }
 
+function EpinglerMsgConv($mid){
+
+    $.ajax({
+        type: "PUT",
+        url: api + "/groups/" + $gid + "/messages/" + $mid + "/pinned",
+        headers : {"authToken" : ""},
+    })
+
+}
+
 
 
 /* GROUPS END */
