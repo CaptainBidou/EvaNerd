8457ff5a19130ba7f665a845903dbf7c8c16ccfb
CaptainBidou <tomas.salvado_robalo@ig2i.centralelille.fr>
2023-03-01 10:10:33 +0100

    likeblanc

--- Front Evanerd/biblio_composants.js
+++ Front Evanerd/biblio_composants.js
@@ -267,7 +267,7 @@ function JcreerPost(Reponse,membre,admin){
     var jClonePostComm=JPostCommentaire.clone(true,true).attr("id_post",Reponse.id);
     var jClonePostLike=JPostLike.clone(true,true).attr("id_post",Reponse.id);
     if(Reponse.liked==1)
-    {jClonePostLike.attr("src","Ressources/Accueil/likeNOIR.png");}
+    {jClonePostLike.attr("src","Ressources/Accueil/likeBlanc.png");}
 
     //qsdqsdqd
 
