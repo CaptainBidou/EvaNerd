488a06519925e14a24a627b54462eb0d3bd77eb3
LukasG62 <lukas.grando.62@gmail.com>
2023-03-11 14:52:23 +0100

    feat(index) ajout route POST /post/id/reactions

--- Back Evanerd/index.php
+++ Back Evanerd/index.php
@@ -202,6 +202,11 @@ switch($action) {
     case "POST /posts/ID/likes":
         postPostLike($data, $idTabs, $authKey);
         break;
+    
+    
+    case "POST /posts/ID/reactions":
+        postPostReactions($data, $idTabs, $authKey, $queryString);
+    break;
 
     case "POST /posts/ID/messages":
         postPostMessage($data, $idTabs, $authKey, $queryString);
