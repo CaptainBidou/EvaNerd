77fb35e4f310b4ca3983d6c75a46fa1bd7e7f082
LukasG62 <lukas.grando.62@gmail.com>
2023-03-13 11:58:10 +0100

    feat(__construct) ajout param error

--- Back Evanerd/includes/Email.php
+++ Back Evanerd/includes/Email.php
@@ -10,14 +10,15 @@
     class Email {
         private $mailer = null;
 
-        public function __construct($host, $username, $password, $port, $sender, $debug = 0) {
-            $this->mailer = new PHPMailer(true);
+        public function __construct($host, $username, $password, $port, $sender, $debug = 0, $error = false) {
+            $this->mailer = new PHPMailer($error);
             if($debug) $this->mailer->SMTPDebug = SMTP::DEBUG_SERVER;
 
             $this->mailer->Host = $host; // Specify main SMTP server
             $this->mailer->Username = $username; // SMTP username
             $this->mailer->Password = $password; // SMTP password
             $this->mailer->Port = $port;
+            // TODO : add TLS optional by parameters 
             $this->mailer->SMTPAuth = true; // Enable SMTP authentication
             $this->mailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption;
             $this->mailer->isSMTP(); // Set mailer to use SMTP
